525a30a801c71872b0b6815e3878a5d918db62c8
Loïc <loic@piernas.fr>
2024-09-18 20:34:51 +0000

    fix entrypoint on install

--- calisum/__main__.py
+++ calisum/__main__.py
@@ -1,6 +1,9 @@
 """ calisum.__main__: executed when calisum directory is called as script. """
 import asyncio
-from calisum.cli import main
+from calisum.cli import main as run_cli
+
+def main():
+    asyncio.run(run_cli())
 
 if __name__ == "__main__":
-    asyncio.run(main())
+   main()
